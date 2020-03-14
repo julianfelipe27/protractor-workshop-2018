3cbfb5903e1bcc84f90909bd13fa96baa9795c3f
Julián Gutiérrez <38226781+julianfelipe27@users.noreply.github.com>
2020-03-14 11:02:30 -0500

    Webdriver (#4)
    * selenium promise manager disabled
    
    * EOF test

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,12 +1,13 @@
 import { Config, browser } from 'protractor';
 import { reporter } from './helpers/reporter';
 
+
 export const config: Config = {
+  SELENIUM_PROMISE_MANAGER:false,
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
-  }
+  },
 };
